9ebc90b15d4a7aad291bea1353e5602b5584adf9
Austin Johnson <technicallyaustin@gmail.com>
2019-12-22 20:05:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,8 +26,6 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 shipping_manifest["oil paintings"]
 end
 
@@ -37,15 +35,5 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest = {"muskets" => 2}
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-  def
-shipping_manifest = {"gun powder" => 4}
-
-
-  # return the shipping_manifest hash below
-shipping_manifest
+  shipping_manifest = {"muskets" => 2,"gun powder" => 4}
 end
